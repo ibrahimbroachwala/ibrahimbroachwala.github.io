48ba50df71d7f0b87fdc85d7ea8f325c9dcd3758
ibrah <ibrahimkb1996@gmail.com>
2017-09-02 16:26:11 +0530

    Update

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+	$('#yourElement').addClass('animated bounceOutLeft');
+
+	$('.parallax').parallax();
+      $(".button-collapse").sideNav({
+      	menuWidth: 200,
+      	draggable: true
+      });
+    });
+
+ $(document).ready(function(){
+      
+    });
