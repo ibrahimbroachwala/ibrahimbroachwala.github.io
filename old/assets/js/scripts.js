9be5474d10ba34de4b7c8fd6d089810cafb87f3f
ibrahimbroachwala <ibrahimkb1996@gmail.com>
2020-08-07 18:07:03 +0530

    old site @ /old

---
+++ old/assets/js/scripts.js
@@ -0,0 +1,34 @@
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+        return this;
+    }
+});
+$(document).ready(function(){
+
+  jQuery('#loading').fadeOut(1000);
+
+	$('.scrollspy').scrollSpy({'scrollOffset': 150});
+	$('.parallax').parallax();
+    $(".button-collapse").sideNav({
+      	menuWidth: 200,
+      	closeOnClick: true,
+      	draggable: true,
+  	});
+
+	$('.percentcircle').percentcircle({
+		animate : true,
+		diameter : 80,
+		guage: 10,
+		coverBg: '#0097a7',
+		fillcolor: '#043023',
+		percentSize: '25px',
+		percentWeight: 'normal',
+	});
+
+
+
+});
